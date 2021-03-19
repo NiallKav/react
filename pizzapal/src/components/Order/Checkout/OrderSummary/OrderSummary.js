1d7cbc90989d413dbf696241f5e05d7076b54cf4
NiallKav <33878447+NiallKav@users.noreply.github.com>
2021-03-19 09:56:43 +0000

    Exercise Part One

--- pizzapal/src/components/Order/Checkout/OrderSummary/OrderSummary.js
+++ pizzapal/src/components/Order/Checkout/OrderSummary/OrderSummary.js
@@ -1,48 +1,66 @@
 import React from "react";
 import { Header, List } from 'semantic-ui-react';
+
 const OrderSummary = (props) => {
-  
-  const toppingIdsArray = [];
-  for(let i in props.toppings){
-      toppingIdsArray.push(props.toppings[i].id);
-  };
 
-  const countOccurrences = (array, value) => array.reduce((count, num) => (num === value ? count + 1 : count), 0);
-
-  const toppingsSummary = [];
-
-  for(let id=0; id<16; id++){
-
-    // use countOccurences to count occurences of each id
-    let toppingCount = countOccurrences(toppingIdsArray, id);
-    if (toppingCount > 0) {
-      const toppingWithCount = {
-        id: id,
-        name: props.menu[id].alt,
-        count: toppingCount
-    };
-
-    toppingsSummary.push(toppingWithCount);
+    const toppingIdsArray = [];
+    for(let i in props.toppings){
+        toppingIdsArray.push(props.toppings[i].id);
     }
-}
-  
+
+    const countOccurrences = (array, value) => array.reduce((count, num) => (num === value ? count + 1 : count), 0);
+
+    const toppingsSummary = [];
+
+    for(let id=0; id<16; id++){
+        let toppingCount = countOccurrences(toppingIdsArray, id);
+
+        if(toppingCount > 0){
+            const toppingWithCount = {
+                id: id,
+                name: props.menu[id].alt,
+                count: toppingCount
+            };
+            toppingsSummary.push(toppingWithCount);
+        }
+    }
+
+    let summary=null;
+
+    if(props.toppings.length > 0){
+        summary = (
+            <div>
+                <Header as='h3'>
+                    Your Pizza: 
+                </Header>
+                <List divided verticalAlign='middle'>
+                    {toppingsSummary.map((topping) => {
+                        return( 
+                            <List.Item key={topping.id}>
+                                {topping.name} : {topping.count}
+                            </List.Item>
+                        )
+                    })}
+                </List>
+                <Header as='h4' className='h4margin'>
+                    Total Price: &euro; {props.price.toFixed(2)}
+                </Header>
+            </div>
+        );
+    }
+    else{
+        summary = (
+            <div>
+                <Header as='h4' className="h4margin">
+                    Start adding some toppings! 
+                </Header>
+            </div>
+        );
+    }
+
   return (
     <div>
-       <Header as='h3'>
-         Your Pizza: 
-        </Header>
-        <List divided verticalAlign='middle'>
-      {toppingsSummary.map((topping) => {
-          return( 
-              <List.Item key={topping.id}>
-                  {topping.name}: {topping.count}
-              </List.Item>
-          )
-      })}
-  </List>
-  <Header as='h4' className='h4margin'>
-      Total Price: &euro; {props.price.toFixed(2)}
-  </Header>
+        {summary}
     </div>
   )
 };
