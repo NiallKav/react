a769cd6091324202327898020d040d0bedcbb60d
NiallKav <33878447+NiallKav@users.noreply.github.com>
2021-03-24 19:55:50 +0000

    YourOrder.js

--- pizzapal/src/containers/YourOrders/YourOrders.js
+++ pizzapal/src/containers/YourOrders/YourOrders.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from '../../axios-orders';
+import OrdersTable from '../../components/OrdersTable/OrdersTable';
 
-const name = (props) => {
+const YourOrders = (props) => {
+
+  const [pastOrdersState, setPastOrdersState] = useState({
+    orders: [],
+    ordersLoaded: false,
+    error: false
+  });
+
+  useEffect(() => {
+            axios.get('/orders.json')
+            .then(response => {
+              // get orders from firebase (returns an object)
+              let ordersObject = response.data;
+
+              // create an empty array
+              let ordersArray = []; 
+
+              // store each order object in the ordersArray
+              for (let i in ordersObject){
+                ordersArray.push(ordersObject[i]);
+              }
+
+              // update the state
+              setPastOrdersState({orders: ordersArray, ordersLoaded: true, error: false});
+            })
+            .catch(error => {
+              setPastOrdersState({orders: pastOrdersState.orders, ordersLoaded: pastOrdersState.ordersLoaded, error: true});
+              console.log(pastOrdersState.error, error);
+    });
+  }, [])
+
+    console.log(pastOrdersState.orders);
+
   return (
     <div>
-      <h1>Your Orders</h1>
+      <OrdersTable orders={pastOrdersState.orders} />
     </div>
   )
 };
 
-export default name;
+export default YourOrders;
