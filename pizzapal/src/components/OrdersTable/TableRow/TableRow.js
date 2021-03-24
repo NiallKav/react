afe944a58b23a1599531cbf531c0580c814390a0
NiallKav <33878447+NiallKav@users.noreply.github.com>
2021-03-24 20:10:19 +0000

    TableRow.js

--- pizzapal/src/components/OrdersTable/TableRow/TableRow.js
+++ pizzapal/src/components/OrdersTable/TableRow/TableRow.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { Table, List } from 'semantic-ui-react';
 
-const name = (props) => {
+const TableRow = (props) => {
+
   return (
-    <div>
-      
-    </div>
+    <Table.Row verticalAlign='top'>
+      <Table.Cell>{props.date}</Table.Cell>
+      <Table.Cell>
+        <List>
+          {props.toppings.map((topping) => {
+            return( 
+              <List.Item key={topping.id}>
+                {topping.name} : {topping.count}
+              </List.Item>
+            )
+          })}
+        </List>
+      </Table.Cell>
+      <Table.Cell>&euro;{props.price.toFixed(2)}</Table.Cell>
+    </Table.Row>
   )
 };
 
-export default name;
+export default TableRow;
